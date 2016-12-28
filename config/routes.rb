b307d8eb18473dc23c81b77e2d40f0f89762ac6e
Alex Hendershott <alex.hendershott@gmail.com>
2016-12-27 19:28:36 -0500

    Fixed add form

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   resources :notes
-  get 'pages/home'
-
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "pages#home"
+
+  get 'pages/home'
+  root to: "notes#show"
 
   get 'note/:id/show_dynamic', to: 'notes#show_dynamic', as: 'show_dynamic'
 end
